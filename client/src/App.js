e3ffbb321a7d33f2d1cbee2572af5357982167b6
Sagar <Sagarchawan4646@gmail.com>
2024-02-02 14:14:32 +0530

    backend deployed url

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://user-client-mgmt.onrender.com/graphql',
   cache,
 });
 
